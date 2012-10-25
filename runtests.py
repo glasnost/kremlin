2748f8c67ff51eb5da0fe252cf3f36d8be6b8a50
Alexandre Gauthier <alex@lab.underwares.org>
2012-10-25 04:17:59 -0400

    Refactor test suite to accomodate Flask peculiarities, add proper main()

--- runtests.py
+++ runtests.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
+
 """
 Kremlin unit tests
 """
 
 import os
-import kremlin
 import unittest
 import tempfile
 import shutil
+import sys
+
 
 class KremlinTestCase(unittest.TestCase):
     """ Kremlin magical test cases and agile scrum buzzwords """
@@ -14,15 +17,30 @@ class KremlinTestCase(unittest.TestCase):
     def setUp(self):
         """ Setup blank database and temporary directories for tests """
 
+        # The module must be imported late because the kremlin package
+        # has some tricky code in __init__.py due to side-effects of the very
+        # convenient decorators Flask provides.
+        # At the very least Python will actually cache this, so it
+        # doesn't have a very large impact in practice.
+        #
+        # FIXME: I'm so sorry
+        import kremlin
+
         # Flip testing bit
         kremlin.app.config['TESTING'] = True
 
         # Disable CSRF during testing
         kremlin.app.config['CSRF_ENABLED'] = False
 
-        # Obtain temporary files
-        #self.db_fd = tempfile.mkstemp(prefix="kremlin-test_", suffix=".db")
-        self.imgdir = tempfile.mkdtemp(prefix="kremlin-test_")
+        try:
+            self.imgdir = tempfile.mkdtemp(prefix="kremlin-test_")
+        except IOError as e:
+            print "I/O Error({0}) during mkdtemp: {1}".format(
+                    e.errno, e.strerror
+                )
+            self.fail()
+
+
         kremlin.app.config['UPLOADED_IMAGES_DEST'] = self.imgdir
 
         # Generate unique secret key for the duration of the test
@@ -41,15 +59,18 @@ class KremlinTestCase(unittest.TestCase):
         shutil.rmtree(self.imgdir)
 
     def _login(self, username, password):
+        """ Perform login with test client """
         return self.app.post('/login', data=dict(
             username=username,
             password=password,
         ), follow_redirects=True)
 
     def _logout(self):
+        """ Perform logout with test client """
         return self.app.get('/logout', follow_redirects=True)
 
     def _register(self, username, email, password, confirm):
+        """ Perform user registration with test client """
         return self.app.post('/register', data=dict(
             username=username,
             email=email,
@@ -57,7 +78,6 @@ class KremlinTestCase(unittest.TestCase):
             confirm=confirm,
         ), follow_redirects=True)
 
-    # Actual unit tests below
 
     def test_basic_auth(self):
         """ Test basic user authentication """
@@ -79,5 +99,57 @@ class KremlinTestCase(unittest.TestCase):
         rv = self._logout()
         assert 'Logged out of Kremlin.' in rv.data
 
+
+
+def usage():
+    """ Print interactive usage help """
+    print "Usage: {0} <configuration file>".format(sys.argv[0], sys.argv[1])
+
+
+def main():
+    """ Program entry point when ran interactively """
+
+    # If configuration is not specified, use sane-ish defaults.
+    # Most settings are actually overriden by the test suite's setUp()
+    # method.
+    if len(sys.argv) < 2:
+        if not os.environ.has_key('KREMLIN_CONFIGURATION'):
+            os.environ['KREMLIN_CONFIGURATION'] = \
+                    os.path.abspath('kremlin-example.cfg')
+        elif len(sys.argv) == 2:
+            if sys.argv[1].startswith("-"):
+                sys.exit(1)
+            else:
+                if os.environ.has_key('KREMLIN_CONFIGURATION'):
+                    print "Warning! Overriding config file environment value!"
+
+                os.environ['KREMLIN_CONFIGURATION'] = sys.argv[1]
+        else:
+            usage()
+            sys.exit(1)
+
+    # Attempt to further heighten the test running experience with
+    # a horrible ASCII banner. Feel free to be hit by a strong feeling
+    # of elation.
+    print """
+               #   # ####  ##### #   # ##### #   # #   #
+               #  #  #   # #     ## ##  #  # #  ## #   #
+               ###   ####  ####  # # #  #  # # # # #####
+               #  #  #     #     #   #  #  # ##  # #   #
+               #   # #     ##### #   # #   # #   # #   #
+
+                   Kremlin Magical Everything System
+               Glasnost Image Board and Boredom Inhibitor
+
+                        ------------------------
+                        - EXECUTING TEST SUITE -
+                        ------------------------
+
+          """
+
+    # Build and run test suite for finer control
+    suite = unittest.TestLoader().loadTestsFromTestCase(KremlinTestCase)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
 if __name__ == '__main__':
-    unittest.main()
+    main()
