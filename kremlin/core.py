2d5026b4927c1323e634c41bf6ddb6e6db742cf9
Cameron Kilgore <ghostfreeman@gmail.com>
2015-07-08 01:08:15 -0400

    Fixed last pagination issue

--- kremlin/core.py
+++ kremlin/core.py
@@ -37,7 +37,7 @@ def url_for_other_page(page):
     args = request.view_args.copy()
     args['page'] = page
     return url_for(request.endpoint, **args)
-    app.jinja_env.globals['url_for_other_page'] = url_for_other_page
+app.jinja_env.globals['url_for_other_page'] = url_for_other_page
 
 @app.route('/logs')
 def logs_index():
