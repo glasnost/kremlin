2e61811d1931f93c3f7b9231debbb400185bcd4b
Alexandre Gauthier <alex@lab.underwares.org>
2017-01-04 02:02:06 -0500

    Basic port of runserver.py to Python 3

--- runserver.py
+++ runserver.py
@@ -15,11 +15,13 @@ import sys
 
 def usage():
     """ Display program usage """
-    print """
+
+    print(
+    """
     Kremlin startup helper program
     Mostly useful for development and testing purposes.
     -------------------------------
-    usage: %s <configfile.cfg>
+    usage: {0} <configfile.cfg>
 
     Configuration file name or path is entirely optional, the default value of
     'kremlin.cfg' will be use should it be omitted. Useful for multiple
@@ -28,17 +30,18 @@ def usage():
     You may also set the environment variable 'KREMLIN_CONFIGURATION' to
     obtain the same result, which also works when running the application by
     other means, such as fcgi, gunicorn or unsupported application servers.
-    """ % (sys.argv[0])
+    """.format(sys.argv[0]))
 
 def main():
     """ Program entry point when ran interactively """
-    print "Kremlin Magical Everything System v 0.0.0-None"
-    print "Copyright (c) Glasnost 2010-2011"
-    print "-----------------------------------------------"
+
+    print("Kremlin Magical Everything System v 0.0.0-None")
+    print("Copyright (c) Glasnost 2010-2011")
+    print("-----------------------------------------------")
 
     # If no configuration is specified, use the default.
     if len(sys.argv) < 2:
-        if not os.environ.has_key('KREMLIN_CONFIGURATION'):
+        if 'KREMLIN_CONFIGURATION' not in os.environ:
             os.environ['KREMLIN_CONFIGURATION'] = \
                     os.path.abspath('kremlin.cfg')
     elif len(sys.argv) == 2:
@@ -47,8 +50,8 @@ def main():
             usage()
             sys.exit(1)
         else:
-            if os.environ.has_key('KREMLIN_CONFIGURATION'):
-                print "Warning! Overriding environment value for config file!"
+            if 'KREMLIN_CONFIGURATION' in os.environ:
+                print("Warning! Overriding environment value for config file!")
 
             os.environ['KREMLIN_CONFIGURATION'] = sys.argv[1]
     else:
@@ -61,20 +64,20 @@ def main():
     # race condition should voice complaints to /dev/null, or submit a
     # patch if it is that important to them.
     if not os.path.isfile(os.environ['KREMLIN_CONFIGURATION']):
-        print "Critical: Specified configuration file %s doesn't exist!" %\
-                (os.environ['KREMLIN_CONFIGURATION'])
+        print("Critical: Specified configuration file %s doesn't exist!" %\
+                (os.environ['KREMLIN_CONFIGURATION']))
         sys.exit(1)
 
-    print "INIT: Using configuration file %s" %\
-            (os.environ["KREMLIN_CONFIGURATION"])
+    print("INIT: Using configuration file %s" %\
+            (os.environ["KREMLIN_CONFIGURATION"]))
 
-    print "Connect to http://127.0.0.1:5000 to access."
-    print "Starting application..."
+    print("Connect to http://127.0.0.1:5000 to access.")
+    print("Starting application...")
 
     # Application is imported at this point because kremlin/__init__.py
     # actually loads the configuration.
     from kremlin import app, db_init
-    print "Setting up database..."
+    print("Setting up database...")
     db_init()
     app.run(debug=True)
 
