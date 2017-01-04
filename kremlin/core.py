62c077a3c95e687eeac348c3dcf10bd1c0f663cf
Alexandre Gauthier <alex@lab.underwares.org>
2017-01-03 23:57:38 -0500

    Fix decorator style for /login route

--- kremlin/core.py
+++ kremlin/core.py
@@ -131,7 +131,7 @@ def add_image():
         return(redirect(url_for("entries_index")))
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """ Login to imageboard """
 
