2325839f25febe3bf28e852893b88cf7bb5d98fd
Alexandre Gauthier <alex@lab.underwares.org>
2017-01-04 04:47:54 -0500

    Use string litterals in example config

--- kremlin/config_defaults.py
+++ kremlin/config_defaults.py
@@ -19,9 +19,9 @@ SECRET_KEY = None # Session secret key
 
 USE_X_SENDFILE = False # Enable web server sendfile support
 
-LOGGER_NAME = "kremlin" # Main logger name
+LOGGER_NAME = 'kremlin' # Main logger name
 
-#SERVER_NAME = "localhost" # Default server name
+#SERVER_NAME = 'localhost' # Default server name
 
 MAX_CONTENT_LENGTH = 32 * 1024 * 1024 # 32 Megabytes
 
